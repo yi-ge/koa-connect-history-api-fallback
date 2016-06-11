d2e581f4dfbd560041ec0b63b96ab00b8eaa17d9
Kristóf Poduszló <kripod@protonmail.com>
2016-06-11 12:39:29 +0200

    refactor(adapter): add support for koa v2

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ function makeKoaConnectHistoryApiFallbackAdapter (options) {
   const middleware = require('connect-history-api-fallback')(options)
   const noop = function () {}
 
-  return function* koaConnectHistoryApiFallbackAdapter (next) {
-    middleware(this, null, noop)
-    yield next
+  return function koaConnectHistoryApiFallbackAdapter (ctx, next) {
+    middleware(ctx, null, noop)
+    return next()
   }
 }
 
